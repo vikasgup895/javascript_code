50d552efabc351eeee618c561a802f927742e2f6
Vikas Gupta <145670731+vikasgup895@users.noreply.github.com>
2025-02-01 18:21:12 +0000

    dom

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -39,3 +39,15 @@ const obj3 = {...obj1, ...obj2}
 // console.log(tinderUser.hasOwnProperty('name'));
 
 
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+//  course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
